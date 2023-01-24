1842aba94423ccc757f6c5de7b9ff7413145a65a
Henric Johansson <s2107575@student.hb.se>
2023-01-24 10:24:38 +0100

    sd

---
+++ OOP 2022/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_2022
+{
+    class Person
+    {
+        private string fnamn;
+        private string enamn;
+        private int ålder;
+
+        
+        public Person(string fnamn = " ", string enamn = " ", int ålder = 0)
+        {
+            this.fnamn = fnamn;
+            this.enamn = enamn;
+            this.ålder = ålder;
+        }
+        public bool Over26()
+        {
+            if (ålder > 26)
+            {
+                return true;
+            }
+            return false;
+        }
+        public string getFnamn() { return fnamn; }
+        public string getEnamn() { return enamn; }
+        public int getÅlder() { return ålder; }
+
+        public void SetÅlder(int newÅlder)
+        {
+            if (newÅlder < 0)
+                ålder = newÅlder;
+            else
+            {
+                Console.WriteLine("Du måste ange en ålder som är över 0");
+            }
+
+            
+         
+        }
+
+
+
+    }
+}
--- OOP 2022/Program.cs
+++ OOP 2022/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace OOP_2022
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Person person1 = new Person("Henric", "Johansson", 32);
+            Person person2 = new Person("Daniel","Noun", 27);
+            Person person3 = new Person("Carl", "Wånsander", 26);
+            Person person4 = new Person("Fredrik", "Linhardt", 26);
+
+            person1.SetÅlder(25);
+            Console.WriteLine(person1.getFnamn() +"\t"+ person1.getEnamn() +"\t"+ person1.getÅlder());    
+            Console.WriteLine(person1.Over26());
+
+            Console.WriteLine(person2.getFnamn() +"\t"+ person2.getEnamn() +"\t"+ person2.getÅlder());
+            Console.WriteLine(person2.Over26());
+
+            Console.WriteLine(person3.getFnamn() +"\t"+ person3.getEnamn() +"\t"+ person3.getÅlder());
+            Console.WriteLine(person3.Over26());
+
+            Console.WriteLine(person4.getFnamn() +"\t"+ person4.getEnamn() +"\t"+ person4.getÅlder());
+            Console.WriteLine(person4.Over26());
+
+            
+            
+
+            Console.ReadLine();
         }
     }
 }
